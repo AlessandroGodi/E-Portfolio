962a3bc89f4e40c3881043d0a64c7f183f005f86
Alessandro <121001806+AlessandroGodi@users.noreply.github.com>
2023-01-04 19:09:03 +1100

    solved famous fizz buss interview question

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 for (let i = 1; i <= 20; ++i) {
-     if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplfied`)
+    }
+    else if (i % 3 === 0) {
          console.log(`${i} -> Frontend`) /* NEED `...` when using ${} */
      }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
 }
