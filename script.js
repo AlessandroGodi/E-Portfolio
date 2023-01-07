06bad414e95ffa4c6102f289c016c702cb6a031f
Alessandro <121001806+AlessandroGodi@users.noreply.github.com>
2023-01-08 08:23:53 +1100

    logging user into an application

--- script.js
+++ script.js
@@ -83,3 +83,61 @@ let newArr = arr.filter((element) => element < 50)
 console.log(newArr)
 
 // returns (3) [20, 30, 40]
+
+
+
+
+// map array changing the values of original array
+
+let dollars = [1, 5, 10, 3]
+
+let cents = dollars.map((element) => {
+    return element * 100
+})
+
+console.log(cents)
+
+// map array without using map function 
+let dollars = [1, 3, 5, 10]
+
+let cents = []
+
+for(let i = 0; i <dollars.length; ++i) {
+    cents.push(dollars[i] * 100);
+}
+
+console.log(cents)
+
+
+
+
+// logging user into an application
+
+let users = [
+    {
+        username: "David",
+        email: "david@frontendsimplified.com",
+        password: 'test234',
+        subscriptionStatus: "VIP",
+        discordId: "David Big Boy",
+        lessonsCompleted: [0, 1],
+    }
+]
+
+function login(email, password) {
+    for (let i = 0; i < users.length; ++i) {
+        if (users[i].email === email) {
+            console.log(users[i]);
+            if (users[i].password === password) {
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('password is incorrect - try again')
+            }
+            return;
+        }
+    }
+    console.log('could not find an email that exists')
+}
+
+login('david@frontendsimplified.com', 'test234')
