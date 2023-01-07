febef49a6509a07929dda1dfb136214efa2d1407
Alessandro <121001806+AlessandroGodi@users.noreply.github.com>
2023-01-07 22:26:26 +1100

    simplified array sintax learnt

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// for (let i = 1; i <= 20; ++i) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//         console.log(`${i} -> Frontend Simplfied`)
-//     }
-//     else if (i % 3 === 0) {
-//          console.log(`${i} -> Frontend`) /* NEED `...` when using ${} */
-//      }
-//     else if (i % 5 === 0) {
-//         console.log(`${i} -> Simplified`)
-//     }
-//     else {
-//         console.log(`${i} -> ${i}`)
-//     }
-// }
+for (let i = 1; i <= 20; ++i) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplfied`)
+    }
+    else if (i % 3 === 0) {
+         console.log(`${i} -> Frontend`) /* NEED `...` when using ${} */
+     }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
+}
 
-// const str = "Frontend Simplified"
+const str = "Frontend Simplified"
 
-// for (let i = 0; i <= str.length; ++i) {
-//     console.log(str[i])
-// }
+for (let i = 0; i <= str.length; ++i) {
+    console.log(str[i])
+}
 
 
 
-// // Function definition
-// function welcomePersonToFES (name) {
-//     // console.log("welcome to FES, DAVID") Back ticks used when ${} in code
-//     console.log(`Welcome to Fes, ${name}`) 
-// }
+// Function definition
+function welcomePersonToFES (name) {
+    // console.log("welcome to FES, DAVID") Back ticks used when ${} in code
+    console.log(`Welcome to Fes, ${name}`) 
+}
 
-// // Call the function
-// welcomePersonToFES ('David');
-// welcomePersonToFES ('Zen');
-// welcomePersonToFES ('Metri');
+// Call the function
+welcomePersonToFES ('David');
+welcomePersonToFES ('Zen');
+welcomePersonToFES ('Metri');
 
 function CelsiusToFahrenheit (Celsius) {
     // let fahrenheit = Celsius * 1.8 + 32 Not necesary to have `let fahrenheit`
@@ -46,3 +46,40 @@ console.log(arr);
 arr.push(200)
 
 console.log(arr);
+
+let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter((element) => {
+    console.log(element)
+    return true;
+})
+
+console.log(newArr)
+
+// returns (5) [20, 30, 40, 50, 100]
+
+
+
+let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter((element) => {
+    console.log(element)
+    if (element < 50) {
+        return true;
+    }
+})
+
+console.log(newArr)
+
+// returns (3) [20, 30, 40]
+
+
+
+
+let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter((element) => element < 50)
+
+console.log(newArr)
+
+// returns (3) [20, 30, 40]
